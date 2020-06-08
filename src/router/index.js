47c188fd6e704a31489a228bc43698f3459442c0
Anderson.Z <maribel.alac@gmail.com>
2020-06-08 10:33:16 +0800

    use vue-admin-template

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,124 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import login from "../components/Login.vue";
-import user from "../components/User.vue"
-import admin from '../components/Admin.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(Router)
 
-const router = new VueRouter({
-  routes: [
-    { path: "/", redirect: "/login" },
-    { path: "/login", component: login },
-    { path: "/home", component: user },
-    {path: "/admin", component: admin}
-  ],
-});
+/* Layout */
+import Layout from '@/layout'
 
-router.beforeEach((to, from, next) => {
-  if(to.path === '/login') return next();
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
 
-  const tokenStr = window.sessionStorage.getItem('token');
-  if(!tokenStr) return next('/login');
-  next();
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+  },
+
+  {
+    path: '/Loan',
+    component: Layout,
+    children: [{
+      path: 'index',
+      name: 'Loan',
+      component: () => import('@/views/loan_mng/index'),
+      meta: { title: 'Loan', icon: 'dashboard' }
+    }]
+  },
+
+  {
+    path: '/CardManagement',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Card Management',
+        component: () => import('@/views/card_mng/index'),
+        meta: { title: 'Card Management', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: '/Info',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Information',
+        component: () => import('@/views/info/index'),
+        meta: { title: 'Information', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: 'external-link',
+    component: Layout,
+    children: [
+      {
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
 
-export default router;
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/views/dashboard/user/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>this is user dashboard</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
